e41cfe65b29833879d4c8c5a18e98bdd47cbeda3
PGzxc <zxc_developer@126.com>
2023-11-01 10:55:42 +0800

    v6.0
    消息-ui

--- src/pages/msg/Msg.tsx
+++ src/pages/msg/Msg.tsx
@@ -1,8 +1,32 @@
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import React from 'react';
+import {withSafeAreaInsets} from "react-native-safe-area-context";
+import {commonStyles} from "../../styles/common";
+import {Tabs} from "@ant-design/react-native";
+import {styles} from "./styles";
 
-const Msg = () => <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Msg</Text>
-</View>;
+/**
+ * desc：消息
+ * @constructor
+ */
+const Msg = () => {
+    const tabs = [
+        {title: '未读消息'},
+        {title: '已读消息'},
+    ]
 
-export default Msg;
+    return (
+        <SafeAreaView style={[commonStyles.safeAreaContainer]}>
+            <Tabs tabs={tabs}>
+                <View style={styles.msgContent}>
+                    <Text>没有未读消息</Text>
+                </View>
+                <View style={styles.msgContent}>
+                    <Text>已读消息</Text>
+                </View>
+            </Tabs>
+        </SafeAreaView>
+    )
+};
+
+export default withSafeAreaInsets(Msg);
---
+++ src/pages/msg/styles.tsx
@@ -0,0 +1,14 @@
+/**
+ * @desc：消息文件样式
+ */
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+
+    msgContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    }
+})
